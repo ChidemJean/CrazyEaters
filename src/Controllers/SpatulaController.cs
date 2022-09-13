fced5994fe2e73add844a8c85c16a3b1cd6cf101
Jean Chidem <jeanchidem2@gmail.com>
2022-09-13 17:33:55 -0300

    [feat] launcher animations

--- src/Controllers/SpatulaController.cs
+++ src/Controllers/SpatulaController.cs
@@ -12,18 +12,28 @@ namespace CrazyEaters.Controllers
         NodePath bullerSlotPath;
 
         [Export]
+        NodePath meshPath;
+
+        [Export]
         NodePath skeletonPath;
+
+        [Export]
+        NodePath animationPlayerPath;
 
         [Export(PropertyHint.Layers3dPhysics)]
         public uint spatulaMask;
 
         Spatial bulletSlot;
         Skeleton skeleton;
+        MeshInstance meshInstance;
+        AnimationPlayer animationPlayer;
 
         [Inject]
         GameManager gameManager;
 
         Vector2? initialPos = null;
+
+        float currentDistance = 0f;
 
         public override void _Ready()
         {
@@ -31,6 +41,8 @@ namespace CrazyEaters.Controllers
 
             bulletSlot = GetNode<Spatial>(bullerSlotPath);
             skeleton = GetNode<Skeleton>(skeletonPath);
+            meshInstance = GetNode<MeshInstance>(meshPath);
+            animationPlayer = GetNode<AnimationPlayer>(animationPlayerPath);
         }
 
         public override void _Input(InputEvent @event)
@@ -57,6 +69,10 @@ namespace CrazyEaters.Controllers
                         initialPos = null;
                         skeleton.RotationDegrees = Vector3.Zero;
                         gameManager.inputMode = GameManager.InputMode.SCENE;
+                        animationPlayer.Play("hold");
+                        float animLength = animationPlayer.GetAnimation("hold").Length / animationPlayer.PlaybackSpeed;
+                        animationPlayer.Advance(animLength - animLength * currentDistance);
+                        currentDistance = 0f;
                     }
                 }
                 return;
@@ -67,8 +83,14 @@ namespace CrazyEaters.Controllers
                     Vector2 mousePos = _event.Position * gameManager.hud.currentScale;
                     Vector2 initial = (Vector2) initialPos;
                     Vector2 aux = new Vector2(initial.x, initial.y + 20f);
+                    currentDistance = Mathf.Clamp(mousePos.DistanceTo(initial) / 200f, 0f, 1f);
+
                     float angle = Mathf.Rad2Deg((initial - mousePos).AngleTo(initial - aux));
                     skeleton.RotationDegrees = new Vector3(0, angle, 0);
+                    animationPlayer.Play("pull");
+                    float animLength = animationPlayer.GetAnimation("pull").Length / animationPlayer.PlaybackSpeed;
+                    animationPlayer.Advance(animLength * currentDistance);
+                    animationPlayer.Stop();
                 }
             }
         }
@@ -83,11 +105,11 @@ namespace CrazyEaters.Controllers
         private void SetBoneRotation(string boneName, Vector3 angle)
         {
             int boneIndex = skeleton.FindBone(boneName);
-            Transform rest = skeleton.GetBoneCustomPose(boneIndex);
+            Transform rest = skeleton.GetBoneRest(boneIndex);
             Transform newPose = rest.Rotated(Vector3.Right, angle.x);
             newPose = newPose.Rotated(Vector3.Up, angle.y);
             newPose = newPose.Rotated(Vector3.Back, angle.z);
-            skeleton.SetBoneCustomPose(boneIndex, newPose);
+            skeleton.SetBonePose(boneIndex, newPose);
         }
    }
 }
