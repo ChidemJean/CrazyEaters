783113e631e902e5088d5203b3a1dfbd91c93ab7
Jean Chidem <jeanchidem2@gmail.com>
2023-02-17 23:30:22 -0300

    [deb] iniciando cli

--- src/Optimization/Hud.cs
+++ src/Optimization/Hud.cs
@@ -15,31 +15,34 @@ namespace CrazyEaters.Optimization
 
         public override void _Ready()
         {
-            
+            Connect("size_changed", this, nameof(OnViewportSizeChanged));
         }
 
         public void ChangeViewport3d(Viewport viewport) {
+            GD.Print("CHANGED VIEW 3D");
             this.currentScale = scaleFactor;
             this.viewport3d = viewport;
             this.viewport3d.GetTexture().Flags = ((uint)Texture.FlagsEnum.Filter);
 
             UpdateViewport3DSize(scaleFactor);
-
-            GetViewport().Connect("size_changed", this, nameof(OnViewportSizeChanged));
         }
 
         public void OnViewportSizeChanged()
         {
+            GD.Print("RESIZED");
             UpdateViewport3DSize(this.currentScale);
         }
 
         public void UpdateViewport3DSize(float value, string type = "LOAD")
         {
-            float scaleFactorClamped = Mathf.Clamp(value, 0.2f, 1f);
-            this.viewport3d.Size = GetViewport().Size * scaleFactorClamped;
-            this.currentScale = scaleFactorClamped;
+            GD.Print("UPDATING VIEW3D: " + value);
+            if (this.viewport3d != null) {
+                float scaleFactorClamped = Mathf.Clamp(value, 0.2f, 1f);
+                this.viewport3d.Size = GetViewport().Size * scaleFactorClamped;
+                this.currentScale = scaleFactorClamped;
 
-            EmitSignal("OnUpdateViewport3DSize", type, this.currentScale);
+                EmitSignal("OnUpdateViewport3DSize", type, this.currentScale);
+            }
         }
     }
 }
